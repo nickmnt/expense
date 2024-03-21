e36a392e06127a44a2f738fc95581ee778c45706
Nick <mtaghavinima@gmail.com>
2024-03-21 16:51:16 -0400

    Added cookie config since we use http

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -36,6 +36,9 @@ internal static class HostingExtensions
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
 
+        builder.Services.ConfigureApplicationCookie(opt => 
+            opt.Cookie.SameSite = SameSiteMode.Lax);
+        
         builder.Services.AddAuthentication();
 
         return builder.Build();
