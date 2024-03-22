e90525d442d8ad61b2b57907df78f129f334abad
Nick <mtaghavinima@gmail.com>
2024-03-21 20:23:49 -0400

    Fixed the migration

--- src/IdentityService/Data/Migrations/20240321212916_InitialCreate.Designer.cs
+++ src/IdentityService/Data/Migrations/20240321212916_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IdentityService.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240321204805_InitialCreate")]
+    [Migration("20240321212916_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
