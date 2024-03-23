274c1d9b8c6361a138973ba861f6703ea761d820
Nick <mtaghavinima@gmail.com>
2024-03-23 18:35:26 -0400

    Added initial connection code for MongoDB

--- src/CategoryService/Program.cs
+++ src/CategoryService/Program.cs
@@ -1,3 +1,7 @@
+using CategoryService.Models;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -7,5 +11,12 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("CategoryDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Category>()
+    .Key(x => x.Id, type: KeyType.Ascending)
+    .CreateAsync();
 
 app.Run();
