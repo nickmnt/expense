427f95ee8e76499c8300bca445172a453c0ab9a8
Nick <mtaghavinima@gmail.com>
2024-03-23 19:58:32 -0400

    Added repository implementation for categories

---
+++ src/CategoryService/Data/CategoryRepository.cs
@@ -0,0 +1,25 @@
+using CategoryService.Models;
+using MongoDB.Entities;
+
+namespace CategoryService.Data;
+
+public class CategoryRepository : ICategoryRepository
+{
+    public async Task<IEnumerable<Category>> GetAllCategories()
+    {
+        var query = DB.Find<Category>();
+        query.Sort(x => x.Ascending(c => c.Name));
+        var result = await query.ExecuteAsync();
+        return result;
+    }
+
+    public Task<Category> GetCategoryById(int id)
+    {
+        return DB.Find<Category>().OneAsync(id);
+    }
+
+    public Task CreateCategory(Category category)
+    {
+        return category.SaveAsync();
+    }
+}
