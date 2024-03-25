e796f2baddc39bc252d44029fdb18beb80d629d9
Nick <mtaghavinima@gmail.com>
2024-03-25 00:38:52 -0400

    Added postgres setup for expense service

--- src/ExpenseService/Program.cs
+++ src/ExpenseService/Program.cs
@@ -1,6 +1,11 @@
+using ExpenseService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
