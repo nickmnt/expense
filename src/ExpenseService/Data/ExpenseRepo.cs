42d5c62b5832eaa6d5dace197dd6be0fe29d18aa
Nick <mtaghavinima@gmail.com>
2024-03-28 17:56:25 -0400

    Added expense repository implementation

---
+++ src/ExpenseService/Data/ExpenseRepo.cs
@@ -0,0 +1,72 @@
+using ExpenseService.Models;
+
+namespace ExpenseService.Data;
+
+public class ExpenseRepo : IExpenseRepo
+{
+    private readonly AppDbContext _context;
+
+    public ExpenseRepo(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public bool SaveChanges()
+    {
+        return _context.SaveChanges() > 0;
+    }
+
+    public IEnumerable<Category> GetAllCategories()
+    {
+        var categories = _context.Categories.ToList();
+
+        return categories;
+    }
+
+    public void CreateCategory(Category category)
+    {
+        if (category == null)
+        {
+            throw new ArgumentNullException(nameof(category));
+        }
+        
+        _context.Categories.Add(category);
+    }
+
+    public bool CategoryExists(int categoryId)
+    {
+        return _context
+            .Categories
+            .Any(c => c.Id == categoryId);
+    }
+
+    public bool ExternalCategoryExists(string externalCategoryId)
+    {
+        return _context.Categories.Any(c => c.ExternalId == externalCategoryId);
+    }
+
+    public IEnumerable<Expense> GetExpensesForCategory(int categoryId)
+    {
+        return _context.Expenses
+            .Where(c => c.CategoryId == categoryId)
+            .OrderBy(e => e.CreatedAt)
+            .ToList();
+    }
+
+    public Expense GetExpense(int expenseId)
+    {
+        return _context.Expenses
+            .FirstOrDefault(e => e.Id == expenseId);
+    }
+
+    public void CreateCommand(int categoryId, Expense expense)
+    {
+        if (expense == null)
+        {
+            throw new ArgumentNullException(nameof(expense));
+        }
+                
+        expense.CategoryId = categoryId;
+        _context.Expenses.Add(expense);
+    }
+}
