114e7379a3da9982c7de49def25cc9c31265bab4
Nick <mtaghavinima@gmail.com>
2024-05-29 20:19:53 -0400

    Added CategoriesController

---
+++ src/ExpenseService/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using ExpenseService.Data;
+using ExpenseService.Dtos;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly IExpenseRepo _repository;
+    private readonly IMapper _mapper;
+
+    public CategoriesController(IExpenseRepo repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet()]
+    public ActionResult<IEnumerable<CategoryReadDto>> GetCategories()
+    {
+        var categories = _repository.GetAllCategories();
+
+        return Ok(_mapper.Map<CategoryReadDto>(categories));
+    }
+}
