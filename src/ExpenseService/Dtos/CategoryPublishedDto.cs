09b05e0e775e682768e3649829500d290146f83d
Nick <mtaghavinima@gmail.com>
2024-05-29 20:19:40 -0400

    Added category DTOs

---
+++ src/ExpenseService/Dtos/CategoryPublishedDto.cs
@@ -0,0 +1,8 @@
+namespace ExpenseService.Dtos;
+
+public class CategoryPublishedDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Event { get; set; }
+}
---
+++ src/ExpenseService/Dtos/CategoryReadDto.cs
@@ -0,0 +1,7 @@
+namespace ExpenseService.Dtos;
+
+public class CategoryReadDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
