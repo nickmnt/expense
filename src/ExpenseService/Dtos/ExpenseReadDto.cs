34fcb2ccc87ef187e94057b1894da9891f29573e
Nick <mtaghavinima@gmail.com>
2024-12-28 05:16:04 -0500

    Fix nullables

--- src/ExpenseService/Dtos/ExpenseReadDto.cs
+++ src/ExpenseService/Dtos/ExpenseReadDto.cs
@@ -6,9 +6,9 @@ public class ExpenseReadDto
 
     public decimal Amount { get; set; }
     
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     public DateTime CreatedAt { get; set; }
 
-    public int CategoryId { get; set; }
+    public int? CategoryId { get; set; }
 }
