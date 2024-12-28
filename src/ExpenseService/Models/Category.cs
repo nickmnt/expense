8ac58f08310744588c79b6ee274ba9b56ee4a087
Nick <mtaghavinima@gmail.com>
2024-12-28 04:21:00 -0500

    Limit external id and name length for performance

--- src/ExpenseService/Models/Category.cs
+++ src/ExpenseService/Models/Category.cs
@@ -8,9 +8,11 @@ public class Category
     public int Id { get; set; }
 
     [Required]
+    [MaxLength(512)]
     public string ExternalId { get; set; }
 
     [Required]
+    [MaxLength(100)]
     public string Name { get; set; }
 
     public IEnumerable<Expense> Expenses { get; set; } = new List<Expense>();
