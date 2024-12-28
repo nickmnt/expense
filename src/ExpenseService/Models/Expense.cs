23f7f514b14cdf159cbdb1c86ddbd3cf821a97e2
Nick <mtaghavinima@gmail.com>
2024-12-28 04:37:27 -0500

    Use nullable for Expense model

--- src/ExpenseService/Models/Expense.cs
+++ src/ExpenseService/Models/Expense.cs
@@ -12,12 +12,12 @@ public class Expense
     public decimal Amount { get; set; }
     
     [MaxLength(250)]
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     [Required]
     public DateTime CreatedAt { get; set; }
 
     public int? CategoryId { get; set; }
 
-    public Category Category { get; set; }
+    public Category? Category { get; set; }
 }
