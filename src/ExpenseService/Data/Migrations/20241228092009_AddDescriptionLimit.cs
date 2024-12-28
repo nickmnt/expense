67be4934dcc7d5f5800a7c430b68d1e29e8fc0f2
Nick <mtaghavinima@gmail.com>
2024-12-28 04:20:27 -0500

    Add migration for limiting description length

---
+++ src/ExpenseService/Data/Migrations/20241228092009_AddDescriptionLimit.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionLimit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Expenses",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Expenses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}
